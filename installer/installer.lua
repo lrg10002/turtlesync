ebd95fd51e0eeb522f1fc719463200ffe1ef452e
Layne Gustafson <lrg10002@gmail.com>
2014-04-27 13:43:27 -0400

    Added stuff.

--- installer/installer.lua
+++ installer/installer.lua
@@ -1,1 +1,19 @@
-print("Beginning instalation process...")
+version = "0.1"
+prefix = "https://raw.githubusercontent.com/lrg10002/turtlesync/master/"
+req = http.get(prefix.."resources/version")
+libVersion = req.readLine()
+req.close()
+
+print("==LrgLib installer version "..version.."==")
+print()
+print("> Latest version of LrgLib: " .. libVersion)
+print("> Checking for preexisting version...")
+if lrg~=nil then
+	print("> LrgLib version " .. lrg.getVersion().. "! Will not install!")
+	return
+end
+
+print ("> None exists. Gathering files to install...")
+req = http.get(prefix.."installer/installInst")
+fti = req.readAll()
+req.close()
