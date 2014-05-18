e663acbd653c5a331a8702257df26d53a1d569c4
Layne Gustafson <lrg10002@gmail.com>
2014-05-18 16:39:44 -0400

    Added progress saving.

--- programs/floor.lua
+++ programs/floor.lua
@@ -4,7 +4,9 @@ args = {...}
 width = tonumber(args[1])
 length = tonumber(args[2])
 
-local function pause()
+local function pause(x,y)
+	fhand = fs.open(".floorProg", "w")
+	fhand.write()
 	term.clear(); term.setCursorPos(1,1)
 	print("Out of materials! Waiting for more...")
 	print("Refill and press any key to continue...")
@@ -12,19 +14,23 @@ local function pause()
 	os.pullEvent("key")
 end
 
+local function findNextOpenSlot(x,y)
+	if turtle.getItemCount(turtle.getSelectedSlot()) < 1 then
+		if turtle.getSelectedSlot() == 16 then
+			pause(x,y)
+			turtle.select(1)
+		else
+			turtle.select(turtle.getSelectedSlot()+1)
+			findNextOpenSlot()
+		end
+	end
+end
+
 for x=1,width do
 	for y=1,length do
-		if turtle.getItemCount(turtle.getSelectedSlot()) < 1 then
-			if turtle.getSelectedSlot() == 16 then
-				pause()
-				turtle.select(1)
-			else
-				turtle.select(turtle.getSelectedSlot()+1)
-			end
-		end
-
+		findNextOpenSlot(x,y)
 		turtle.placeDown()
-		if x~=width then turtle.forward() end
+		if y~=length then turtle.forward() end
 	end
 
 	if x%2 == 0 then
