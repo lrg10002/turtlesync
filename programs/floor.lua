73f6a836b479f9063cd232a6806f86f3533ac0eb
Layne Gustafson <lrg10002@gmail.com>
2014-05-18 21:19:06 -0400

    Completed floor program.

--- programs/floor.lua
+++ programs/floor.lua
@@ -1,12 +1,13 @@
 
 args = {...}
 
-width = tonumber(args[1])
-length = tonumber(args[2])
+tWidth = 0
+tLength = 0
 
 local function pause(x,y)
 	fhand = fs.open(".floorProg", "w")
-	fhand.write()
+	fhand.write(textutils.serialize({["x"]=x, ["y"]=y, ["w"]=tWidth, ["l"]=tLength}))
+	fhand.close()
 	term.clear(); term.setCursorPos(1,1)
 	print("Out of materials! Waiting for more...")
 	print("Refill and press any key to continue...")
@@ -26,20 +27,39 @@ local function findNextOpenSlot(x,y)
 	end
 end
 
-for x=1,width do
-	for y=1,length do
-		findNextOpenSlot(x,y)
-		turtle.placeDown()
-		if y~=length then turtle.forward() end
-	end
 
-	if x%2 == 0 then
-		turtle.turnLeft()
-		turtle.forward()
-		turtle.turnLeft()
-	else
-		turtle.turnRight()
-		turtle.forward()
-		turtle.turnRight()
+function run(x,y,width,length)
+	tWidth = width
+	tLength = length
+	for x=1,width do
+		for y=1,length do
+			findNextOpenSlot(x,y)
+			turtle.placeDown()
+			if y~=length then turtle.forward() end
+		end
+
+		if x%2 == 0 then
+			turtle.turnLeft()
+			turtle.forward()
+			turtle.turnLeft()
+		else
+			turtle.turnRight()
+			turtle.forward()
+			turtle.turnRight()
+		end
 	end
 end
+
+if args[1]=="resume" then
+	fhand = fs.open(".floorProg", "r")
+	conts = fhand.readAll()
+	fhand.close()
+	res = textutils.unserialize(conts)
+	run(res.x, res.y, res.w, res.l)
+	return
+end
+
+width = tonumber(args[1])
+length = tonumber(args[2])
+
+run(1,1,width,length)
